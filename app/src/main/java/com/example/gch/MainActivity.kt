295497b0615e65735bf7eb4f93e9931fb7261a52
kjebali23 <khalil.jebali.kj@gmail.com>
2023-11-19 13:47:54 +0100

    Api consumption

--- app/src/main/java/com/example/gch/MainActivity.kt
+++ app/src/main/java/com/example/gch/MainActivity.kt
@@ -10,6 +10,10 @@ import com.example.gch.fragment.FormationFragment
 import com.example.gch.fragment.QuizFragment
 import com.example.gch.fragment.CertifFragment
 import android.util.Log
+import com.example.gch.Models.Formation
+import com.google.gson.Gson
+import com.google.gson.JsonParser
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity(), HttpGetRequest.OnTaskCompleted {
 
@@ -23,24 +27,24 @@ class MainActivity : AppCompatActivity(), HttpGetRequest.OnTaskCompleted {
 
         binding.btnFormation.setOnClickListener {
             // Make HTTP request when the Formation button is clicked
-            val url = "https://192.168.104.196:3000/getAllformation"
+            val url = "http://172.16.5.212:3000/getAllformation"
             val getRequest = HttpGetRequest(this)
             getRequest.execute(url)
 
             // You can also pass data to the FormationFragment if needed
             val formationFragment = FormationFragment()
-            val bundle = Bundle()
-            bundle.putString("someKey", "someValue")
-            formationFragment.arguments = bundle
+//            val bundle = Bundle()
+//            bundle.putString("formations", formationList)
+//            formationFragment.arguments = bundle
 
             changeFragment(formationFragment, "")
         }
 
         binding.btnQuiz.setOnClickListener {
-            // Make HTTP request when the Quiz button is clicked
-//            val url = "http://your-nodejs-server-url/api/quiz"
-//            val getRequest = HttpGetRequest(this)
-//            getRequest.execute(url)
+//             Make HTTP request when the Quiz button is clicked
+            val url = "http://172.16.5.212:3000/getAllquizes"
+            val getRequest = HttpGetRequest(this)
+            getRequest.execute(url)
 
             changeFragment(QuizFragment(), "")
         }
@@ -65,11 +69,48 @@ class MainActivity : AppCompatActivity(), HttpGetRequest.OnTaskCompleted {
     }
 
     override fun onTaskCompleted(result: String) {
-        // Handle the result from the server
-        // Update UI or perform any other actions
-        Log.d("HTTP_RESPONSE", result ?: "Empty response")
+//        // Handle the result from the server
+//        val formationList = Gson().fromJson<List<Formation>>(result)
+//        // Update UI or perform any other actions
+//        Log.d("HTTP_RESPONSE", result ?: "Empty response")
+//
+////        binding.testText.text = result ?: "Empty response"
 
-//        binding.testText.text = result ?: "Empty response"
+        if (!isValidJSON(result)) {
+            Log.e("Error", "Invalid JSON data")
+            return
+        }
+
+        try {
+            val jsonElement = JsonParser.parseString(result)
+            val formationList: List<Formation> = Gson().fromJson(jsonElement, object : TypeToken<List<Formation>>() {}.type)
+            // Update UI or perform any other actions
+            Log.d("HTTP_RESPONSE", result ?: "Empty response")
+
+            for (formation in formationList) {
+                Log.d("Formation", "------------------------------")
+                Log.d("Formation ID", formation.id.toString())
+                Log.d("Formation Name", formation.title)
+                Log.d("Formation Description", formation.description)
+//                Log.d("Formation Image URL", formation.imageUrl)
+            }
+
+
+
+            // Use formationList to update UI or perform actions
+        } catch (e: Exception) {
+            Log.e("Error", "Failed to parse JSON data: ${e.message}")
+        }
+
+    }
+
+    private fun isValidJSON(jsonString: String): Boolean {
+        try {
+            JsonParser.parseString(jsonString)
+            return true
+        } catch (e: Exception) {
+            return false
+        }
     }
 
     override fun onTaskError(error: Exception) {
